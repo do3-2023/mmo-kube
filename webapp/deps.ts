f7a04b2d14df1d935aabd9b08cb2efd1897e1e3d
Martin Moreira de Jesus <martin.moreira-de-jesus@clicnwork.io>
2023-07-02 14:18:40 +0200

    feat(webapp): add healthcheck, and index

--- webapp/deps.ts
+++ webapp/deps.ts
@@ -1,3 +1,9 @@
-export { serve } from "https://deno.land/std@0.192.0/http/server.ts";
+export {
+  Application,
+  Router,
+  Context,
+} from "https://deno.land/x/oak@v12.5.0/mod.ts";
 
 export { renderFileToString } from "https://deno.land/x/dejs@0.10.3/mod.ts";
+
+export { config } from "https://deno.land/x/dotenv@v3.2.2/mod.ts";
--- webapp/main.ts
+++ webapp/main.ts
@@ -1,19 +1,54 @@
-import { serve } from "./deps.ts";
+import { config } from "https://deno.land/x/dotenv@v3.2.2/mod.ts";
+import { Application, Router, Context } from "./deps.ts";
 import { renderFileToString } from "./deps.ts";
+
+interface ApiResponse {
+  count: number;
+  last_call: Date | null;
+  timpestamp: Date;
+}
 
 const port = 8080;
 
-const handler = async (): Promise<Response> => {
-  const myTemplate = await renderFileToString("index.ejs", {
-    message: "Hello world",
-  });
-  return new Response(myTemplate, {
-    status: 200,
-    headers: {
-      "Content-Type": "text/html",
-    },
-  });
-};
+// load .env
+config({ export: true });
 
-console.log(`HTTP webserver running. Access it at: http://localhost:8080/`);
-await serve(handler, { port });
+const apiUrl = Deno.env.get("API_URL");
+
+if (!apiUrl) {
+  console.error("Error: API_URL environment variable missing");
+  Deno.exit(1);
+}
+
+const app = new Application();
+const router = new Router();
+
+router.get("/", async (ctx: Context) => {
+  const response = await fetch(apiUrl);
+
+  const data: ApiResponse = await response.json();
+
+  const myTemplate = await renderFileToString("index.ejs", {
+    ...data,
+  });
+
+  ctx.response.body = myTemplate;
+  ctx.response.status = 200;
+  ctx.response.headers.set("Content-Type", "text/html");
+});
+
+router.get("/healthz", async (ctx: Context) => {
+  try {
+    await fetch(`${apiUrl}/healthz`);
+    ctx.response.status = 204;
+  } catch (_) {
+    ctx.response.status = 500;
+  }
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+console.log(`Server is running on http://localhost:${port}`);
+
+await app.listen({ port });
